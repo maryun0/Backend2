540043773e776ffb7f36137f5ad67cab4ab38ed7
Marcelo Nogai <yuassa11@gmail.com>
2024-03-05 17:47:09 -0300

    asd

--- Persistencia/Conexao.js
+++ Persistencia/Conexao.js
@@ -1,33 +1,22 @@
-//O banco de dados escolhido será o MYSQL
 import mysql from 'mysql2/promise';
 
-//Nós vamos desenvolver código assíncrono, já que a nossa aplicação
-//não é o banco de dados e sim se comunica com um banco de dados que pode
-//responder de imediato, demorar para responder ou nunca respoder
-//por isso vamos usar o async e o await
+// Utilize um módulo ou escopo fechado para armazenar o pool
+let pool;
 
 export default async function conectar() {
-    if (global.pool !== undefined) {
-        return await global.pool.getConnection();
-    }
-    else {
-        const pool = mysql.createPool({
-            host: 'localhost',
-            user: 'root', //não é recomendado usar o super usuário
-            password: '',
-            port: 3307,
-            database: 'backend',
-            waitForConnections: true,
-            connectionLimit: 10,
-            maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
-            idleTimeout: 360000, // idle connections timeout, in milliseconds, the default value 60000
-            queueLimit: 0,
-            enableKeepAlive: true,
-            keepAliveInitialDelay: 0,
-        });
+    if (pool) return await pool.getConnection();
 
-        //garantindo que haja somente uma cópia desse pool para a minha aplicação
-        global.pool = pool;
-        return await pool.getConnection();
-    }
+    pool = mysql.createPool({
+        host: 'localhost',
+        user: 'usuario_app', // Usuário específico para a aplicação
+        password: 'senha_segura', // Senha segura
+        port: 3306,
+        database: 'backend2',
+        waitForConnections: true,
+        connectionLimit: 10,
+        idleTimeout: 360000, // Tempo limite para conexões ociosas
+        queueLimit: 0,
+    });
+
+    return await pool.getConnection();
 }
